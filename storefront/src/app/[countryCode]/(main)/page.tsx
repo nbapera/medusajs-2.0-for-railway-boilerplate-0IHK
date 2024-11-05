8a7b112c836c47e4a4a4f443e773b37ad8647f94
nbapera <valvejegovno@gmail.com>
2024-11-05 23:38:06 +0100

    test

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -25,6 +25,7 @@ export default async function Home({
 
   return (
     <>
+      <Hero />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
